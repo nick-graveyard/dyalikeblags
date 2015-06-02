b873349c191e6573f1d91a0a160170df91be199f
Nick <nickkiermaier@gmail.com>
2015-06-01 21:57:06 -0500

    moar

--- app/controllers/dyalikeblags/dashboard/users_controller.rb
+++ app/controllers/dyalikeblags/dashboard/users_controller.rb
@@ -2,7 +2,7 @@ require_dependency "dyalikeblags/dashboard_controller"
 
 module Dyalikeblags
   class Dashboard::UsersController < DashboardController
-    #efore_action :set_user, only: [:show, :edit, :update, :destroy]
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
 
     # GET /users
     def index
@@ -13,17 +13,12 @@ module Dyalikeblags
  
     # GET /users/1
     def show
-      @user = ::User.find(params[:id])
     end
 
 
-   end
-end
-
-=begin
     # GET /users/new
     def new
-      @user = main_app.User.new
+      @user = ::User.new
     end
 
     # GET /users/1/edit
@@ -32,10 +27,10 @@ end
 
     # POST /users
     def create
-      @user = main_app.User.new(user_params)
+      @user = ::User.new(user_params)
 
       if @user.save
-        redirect_to @user, notice: 'User was successfully created.'
+        redirect_to [:dashboard, @user], notice: 'User was successfully created.'
       else
         render :new
       end
@@ -44,7 +39,7 @@ end
     # PATCH/PUT /users/1
     def update
       if @user.update(user_params)
-        redirect_to @user, notice: 'User was successfully updated.'
+        redirect_to [:dashboard, @user], notice: 'User was successfully updated.'
       else
         render :edit
       end
@@ -53,18 +48,19 @@ end
     # DELETE /users/1
     def destroy
       @user.destroy
-      redirect_to users_url, notice: 'User was successfully destroyed.'
+      redirect_to dashboard_users_path, notice: 'User was successfully destroyed.'
     end
 
     private
       # Use callbacks to share common setup or constraints between actions.
       def set_user
-        #@user = main_app.User.find(params[:id])
+        @user = ::User.find(params[:id])
       end
 
       # Only allow a trusted parameter "white list" through.
       def user_params
-        #params[:user]
+        params.require(:user).permit(:email, :username, :password, :password_confirmation) 
       end
-=end
+end
+end
 
